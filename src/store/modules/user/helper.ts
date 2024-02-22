5ce6133987a6e82f568e15f40df8b895fe9fcbcf
typeopen <75972315+typeopen@users.noreply.github.com>
2024-02-22 08:34:11 +0800

    Update helper.ts

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -15,9 +15,9 @@ export interface UserState {
 export function defaultSetting(): UserState {
   return {
     userInfo: {
-      avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
-      name: 'ChenZhaoYu',
-      description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >GitHub</a>',
+      avatar: 'https://0-1252454288.cos.ap-chengdu.myqcloud.com/123.svg',
+      name: '无尽苍白',
+      description: '微信：Hydra_Wechat',
     },
   }
 }
